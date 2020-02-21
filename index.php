c779204e94e2ac58b0da436d8532b50a41bd69c6
Imelda Medina <imedina-castaneda@mail.greenriver.edu>
2020-02-21 12:57:43 -0500

    adding dropdown lists and arrays

--- index.php
+++ index.php
@@ -18,7 +18,10 @@ $f3->set('DEBUG', 3);
 
 //Define arrays
 $f3->set('purposes', array('Multipurpose', 'Warship', 'Passenger Liner', 'Luxury'));
-
+$f3->set('colors', array('red', 'black', 'silver', 'white', 'gold', 'yellow'));
+$f3->set('shieldings', array('s-0001-A','s-0001-B','s-0002-A','s-0002-B','s-0002-C','s-????-A','s-????-B'));
+$f3->set('engines',array());
+$f3->set('hyperdrives',array());
 //define a default route
 $f3->route('GET|POST /log', function(){
     $view = new Template();
@@ -34,15 +37,25 @@ $f3->route('GET|POST /customize', function($f3){
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         //Get the data from the form
         $purpose = $_POST['purpose'];
-
+        $color= $_POST['color'];
+        $shielding = $_POST['shielding'];
+        $engine = $_POST['engine'];
+        $hyperdrive = $_POST['hyperdive'];
         //Add data to hive
         $f3->set('purp',$purpose);
-
+        $f3->set('col',$color);
+        $f3->set('shield',$shielding);
+        $f3->set('eng',$engine);
+        $f3->set('hyper',$hyperdrive);
         //If data is valid
         if(validForm()){
 
             //write data to session
             $_SESSION['purp'] = $purpose;
+            $_SESSION['col'] = $color;
+            $_SESSION['shield'] =$shielding;
+            $_SESSION['eng'] =$engine;
+            $_SESSION['hyper'] =$hyperdrive;
             //redirect to finalize
             $f3->reroute('/finalize');
         }
